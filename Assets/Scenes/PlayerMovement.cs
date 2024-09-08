7d8fc7829c4c9e156d6a04620255cc3995c8b63c
smoskii <cmyk05@gmail.com>
2024-09-08 11:48:18 -0700

    Added movable player

---
+++ Assets/Scenes/PlayerMovement.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public Rigidbody2D rb;
+
+    private Vector2 moveDirection;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Processing Inputs
+        ProcessInputs();
+    }
+
+    void FixedUpdate()
+    {
+        // Physics calculations
+        Move();
+    }
+
+    void ProcessInputs()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDirection = new Vector2(moveX, moveY).normalized;
+    }
+
+    void Move()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+}
